1bd9891f2c35e9da3f80f3d78393f620741623b2
Elías Gabriel Pérez <eg642616@gmail.com>
2025-04-14 13:57:39 -0600

    Initial commit.

---
+++ init-file-creator.el
@@ -0,0 +1,102 @@
+;;; init-file-creator.el --- Create a config with popular packages or common configurations.  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2025 Free Software Foundation, Inc.
+
+;; Author: Elijah Gabe Pérez <eg642616@gmail.com>
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This package allows you create an Emacs configuration quickly on an
+;; Easy Customization interface.
+;;
+;; The configuration created is simple to allow it to be easily
+;; configurable and easy to learn.
+;;
+;; This allows install and configure popular packages from
+;; GNU and NonGNU ELPA, configure and enable or disable built-in
+;; features.
+
+;;; Code:
+(require 'cus-edit)
+(require 'init-file-creator-packages)
+
+(defvar init-creator--buffer-name "*Init file Creator*")
+
+;; TODO: A better description about buffer contents.
+(defvar init-creator--basic-buffer-info
+  (concat
+   (propertize "Init Configuration Creator" 'face 'info-title-1)
+   (propertize "\n\nWelcome to Emacs init file creator mode.  \
+This will guide you to create a basic and quick init file.
+
+This mode will let you install and configure popular packages from Package Archives
+and set built-in features or modes.\n\n"
+               'face 'variable-pitch))
+  "String to insert top init creator buffer.")
+
+(defvar-keymap init-file-creator-map
+  :doc "Keymap used in the \"*Init file Creator*\" buffer."
+  :full t
+  :parent widget-keymap
+  "SPC"     #'scroll-up-command
+  "S-SPC"   #'scroll-down-command
+  "DEL"     #'scroll-down-command
+  ;; "C-x C-s" #'
+  "q"       #'Custom-buffer-done
+  "n"       #'widget-forward
+  "p"       #'widget-backward)
+
+;; TODO: set tool bar buttons.
+(define-derived-mode init-file-creator-mode nil "Init file creator"
+  "Major mode for create Emacs init file."
+  (kill-all-local-variables)
+  (buffer-disable-undo)
+  (let ((inhibit-read-only t))
+    (erase-buffer))
+  (remove-overlays)
+  (custom--initialize-widget-variables)
+  (use-local-map init-file-creator-map))
+
+(defmacro init-creator--with-buffer (&rest body)
+  "Execute BODY in *Init file Creator* buffer."
+  `(progn
+     (switch-to-buffer init-creator--buffer-name)
+     (init-file-creator-mode)
+     (save-excursion
+       ,@body)
+     (widget-setup)))
+
+(defun create-init-file ()
+  "Create an init configuration file."
+  (interactive)
+  (init-creator--with-buffer
+   (widget-insert init-creator--basic-buffer-info)
+   (widget-create 'editable-field
+                  :format "Where to create init file: %v "
+                  (expand-file-name "~/.config/emacs/init.el"))
+   (widget-insert ?\n)
+   (widget-create 'push-button :tag "Create configuration"
+                  :help-echo "Push me when done customizing."
+                  :action 'init-creator--create-init-file)
+   (widget-insert ?\n)
+   (custom-group--draw-horizontal-line)
+   (mapc (lambda (fn) (funcall fn)) init-creator--config-widgets)
+   ))
+
+(provide 'init-file-creator)
+;;; init-file-creator.el ends here
